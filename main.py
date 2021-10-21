8fcf0eaf5728255d732145e4fc13c9777c0575db
Konosprod <konosprod@free.fr>
2021-10-22 00:54:36 +0200

    Parse game title, and use Cutlet to create romaji title
    Signed-off-by: Konosprod <konosprod@free.fr>

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
 from bs4 import BeautifulSoup
 import requests
+import cutlet
 
-BASE_URL = "https://www.freem.ne.jp/"
-BASE_PAGE_URL = BASE_URL + "win/category/4/page-"
+#Romaji translation without using foreign spelling
+katsu = cutlet.Cutlet()
+katsu.use_foreign_spelling = False
+
+BASE_URL = "https://www.freem.ne.jp"
+BASE_PAGE_URL = BASE_URL + "/win/category/4/page-"
 page = 0
 
 def get_source(url):
     r = requests.get(url)
     return BeautifulSoup(r.text, "html.parser")
 
+def get_games(src):
+    listgame = src.find("ul", {"class":"game-list-wrap"})
+    urls = []
+    for game in listgame.findChildren("li", recursive=False):
+        if "sp-ad-list" not in game.get("class"):
+            urls.append(BASE_URL + game.find("a")["href"])
+
+    return urls
+
+def get_game_info(src):
+    game = {}
+    
+    game["title"] = src.find("h1").getText().strip()
+    game["title_romaji"] = katsu.romaji(game["title"])
+
+    return game
+
 def main():
     #src = get_source(BASE_PAGE_URL + str(page))
     src = ""
-    with open("index.html", "r", encoding="utf-8") as f:
+    with open("game.html", "r", encoding="utf-8") as f:
         src = BeautifulSoup(f.read(), "html.parser")
-    
-    listgame = src.find("ul", {"class":"game-list-wrap"})
 
-    for game in listgame.findChildren("li", recursive=False):
-        if "sp-ad-list" not in game.get("class"):
-            print(game.find("a")["href"])
+        print(get_game_info(src))
+
+
 
 if "__main__" == __name__:
     main()
